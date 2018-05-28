2f6f59e1da59d89c2d08ce90836fd66cb7c8429a
johnn <johnnymhanly@gmail.com>
2018-05-28 15:29:29 -0700

    adding hamburger button

--- scripts/components/container.js
+++ scripts/components/container.js
@@ -3,29 +3,39 @@ import Funny from './funny';
 import Motivation from './motivation';
 import Books from './books';
 import Thought from './thought';
+import { slide as Menu } from 'react-burger-menu'
 
 
 
 class Container extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            
-         }
-    }
-    render() { 
-        return ( 
+        this.state = {
 
-        <div>
-            <h1></h1>
-            <Thought />
-            <Books />
-            <Motivation />
-            <Funny />
-        </div>
-         )
+        }
+    }
+  
+
+
+    render() {
+        return (
+        
+
+                    <Menu>
+                        <a id="Home" className="menu-item" href="/">Home</a>
+                        <a id="Movivational" className="menu-item" href="/">Motivational</a>
+                        <a id="Funny" className="menu-item" href="/">Funny</a>
+                        <a id="Books" className="menu-item" href="/">Books/Movies</a>
+                        <a id="Thought" className="menu-item" href="/">Thought-Provoking</a>
+                    </Menu>
+
+               
+
+
+
+        );
     }
 }
- 
+
 
 export default Container;
