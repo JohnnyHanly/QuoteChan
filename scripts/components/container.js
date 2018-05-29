4838dbb2a475945cbc9264428351fcf9fdb01766
johnn <johnnymhanly@gmail.com>
2018-05-29 10:40:24 -0700

    adding first version of component switcher

--- scripts/components/container.js
+++ scripts/components/container.js
@@ -11,52 +11,73 @@ class Container extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            containerVisible:true,
-            booksVisible:false,
-            funnyVisible:false,
-            thoughtVisible:false,
-            motivationVisible:false
+            containerVisible: true,
+            booksVisible: false,
+            funnyVisible: false,
+            thoughtVisible: false,
+            motivationVisible: false
 
         }
     }
-  
-     toggleHidden(toggle){
-         this.setState({
-             toggle:!toggle
-             
-         })
-     }
+    componentSwitcher(boolean) {
+        // console.log(event);
+        console.log(boolean);
+        
+        this.setState((prevState) => {
+            return {
+                
+                [boolean]: !prevState[boolean]
+            }
+        })
+ 
+    }
+ 
+    // flagSwitcher(boolean){
+    //     switch(boolean){
+    //         case motivationVisible:
+    //         return <Motivation />;
+    //         case booksVisible:
+    //         return <Books />;
+    //         case thoughtVisible:
+    //         return <Thought />;
+    //         case funnyVisible:
+    //         return <Funny />;
+    //         default:
+    //         return <Container />
+
+    //     }
+    // }
 
     render() {
         return (
             <div>
-        
-                    <Menu>
-                        <a id="Home" className="menu-item">Home</a>
-                        <a onClick={this.toggleHidden.bind(this.state.motivationHidden)}id="Motivational" className="menu-item" href="/">Motivational</a>
-                        <a id="Funny" className="menu-item" href="/">Funny</a>
-                        <a id="Books" className="menu-item" href="/">Books/Movies</a>
-                        <a id="Thought" className="menu-item" href="/">Thought-Provoking</a>
-                    </Menu>
-                    
-                     <div>
-                     {
-                    
 
+                <Menu>
+                    <li onClick={()=>this.componentSwitcher("containerVisible")} id="Home" className="menu-item">Home</li>
+                    <li onClick={()=>this.componentSwitcher("motivationVisible")}  id="Motivational" className="menu-item" href="/">Motivational</li>
+                    <li onClick={()=>this.componentSwitcher("funnyVisible")}  id="Funny" className="menu-item" href="/">Funny</li>
+                    <li onClick={()=>this.componentSwitcher("booksVisible")} id="Books" className="menu-item" href="/">Books/Movies</li>
+                    <li onClick={()=>this.componentSwitcher("thoughtVisible")}  id="Thought" className="menu-item" href="/">Thought-Provoking</li>
+                </Menu>
 
+                {/* in progress
+                {<div>
+                    {(this.booksVisible ? <Books /> :
+                        (this.funnyVisible ? <Funny /> :
+                            (this.thoughtVisible ? <Thought /> :
+                                (this.motivationVisible ? <Motivation /> :
+                                    <Container />
+                                )
+                            )
+                        )
+                    )
+                    }
 
-
-                     } 
-                     
-
-
-                        </div>  
-                   
-
+                </div> } */}
 
             </div>
-            
-            
+
+
 
         );
     }
